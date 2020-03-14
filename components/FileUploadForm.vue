f9a86689c8cec177580a97af27a950b1971de46f
Fred Buchanan <oscillator.b@gmail.com>
2020-03-14 16:24:30 -0400

    Added file uploader components.

---
+++ components/FileUploadForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-form-file
+      v-model="file"
+      :state="Boolean(file)"
+      placeholder="Choose an audio file or drop it here..."
+      drop-placeholder="Drop file here..."
+      accept=".wav"
+    />
+    <b-button @click="submit">Upload!</b-button>
+  </div>
+</template>
+<script>
+    export default {
+        name: 'FileUploadForm',
+        data() {
+            return {
+                file: null,
+            }
+        },
+        methods: {
+            submit() {
+                this.$emit('new-file', this.file)
+            },
+        },
+    }
+</script>
---
+++ components/FirebaseFileUpload.vue
@@ -0,0 +1,23 @@
+<template>
+  <FileUploadForm @new-file="onFileUpload"/>
+</template>
+
+<script>
+    import FileUploadForm from './FileUploadForm'
+
+    export default {
+        name: "FirebaseFileUpload",
+        components: {
+            FileUploadForm,
+        },
+        methods: {
+            onFileUpload(file) {
+                console.log(file)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
